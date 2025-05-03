217e5b58bb2d1aba2f86f9d3a0f8748dc2ced9e3
Akash <thakare.akash007@gmail.com>
2025-05-03 23:34:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return <>
+    <h1 className="text-3xl font-bold underline bg-red-600">Welcome to the React App</h1>
+    <p className="">This is a simple React application.</p>
+    <p>It is designed to demonstrate the basic structure of a React app.</p>
+  </>;
+}
+
+export default App;
